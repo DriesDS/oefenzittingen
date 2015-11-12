420742225870f04f978f3f4d69aa097bc36d9fe3
Dries_DS <dries.ds@hotmail.com>
2015-11-12 15:43:10 +0100

    ginibre matrix made

--- huistaak5/cholesky.f90
+++ huistaak5/cholesky.f90
@@ -1,8 +1,39 @@
-program main
+program cholesky
 
 implicit none
 
+call ginibre()
+
 contains
 
-end program
+	subroutine ginibre()
+		real, dimension(7,7) :: A, B
+		complex, dimension(7,7) :: C, C1, C2
 
+		call random_seed(2568)
+		call random_number(A)
+		call random_number(B)
+		call gaussian(A,B,C1)
+
+		write(*,'(7(e12.3,/)') A
+		write(*,'(7(e12.3,/)') B
+		write(*,'(7(e12.3,/)') C1
+
+		C2 = transpose(C1)
+		C2 = conjg(C2)
+
+		C = matmul(C1,C2)
+
+		write(*,'(7(e12.3,/)') C
+
+	end subroutine
+
+	ELEMENTAL subroutine gaussian(a, b, c)
+		intent(in) :: a, b
+		intent(out) :: c
+
+		c = sqrt(-2*log(a))*exp(8*atan(1.0)*sqrt(-1)*b)
+
+	end subroutine
+
+end program
